832da42788022f6ce57ae58d5f5e8d13c2179142
Doug Henderson <djhenderson@users.noreply.github.com>
2017-06-20 19:01:11 -0600

    expand info in setup.py and PKG-INFO

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 #
 # Setup script for the minixsv library
 #
@@ -8,11 +9,12 @@
 from distutils.core import setup
 #from setuptools import setup
 
-LONG_DESCRIPTION = '''minixsv is a lightweight XML schema validator written in "pure" Python
-Currently a subset of the XML schema standard is supported
-minixsv is based on genxmlif, a generic XML interface class,
-which currently supports minidom, elementtree or 4DOM/pyXML as XML parser
-Other parsers can be adapted by implementing an appropriate derived interface class
+LONG_DESCRIPTION = '''\
+minixsv is a lightweight XML schema validator written in "pure" Python
+Currently a subset of the XML schema standard is supported minixsv is
+based on genxmlif, a generic XML interface class, which currently
+supports minidom, elementtree or 4DOM/pyXML as XML parser Other parsers
+can be adapted by implementing an appropriate derived interface class
 '''
 
 setup(name='minixsv',
@@ -22,18 +24,27 @@ setup(name='minixsv',
       author='Roland Leuthe',
       author_email='roland@leuthe-net.de',
       url='http://www.leuthe-net.de/MiniXsv.html',
-      packages=['minixsv', 'genxmlif'],
       license='Python (MIT style)',
       platforms='Python 2.4 and later',
       classifiers=["Development Status :: 4 - Beta",
+                   "Intended Audience :: Developers",
+                   "License :: OSI Approved :: Python (MIT style)",
                    "Operating System :: OS Independent",
-                   "Topic :: Text Processing :: Markup :: XML",],
+                   "Programming Language :: Python",
+                   "Programming Language :: Python :: 2",
+                   "Programming Language :: Python :: 2.7",
+                   "Programming Language :: Python :: 3",
+                   "Programming Language :: Python :: 3.6",
+                   "Topic :: Text Processing :: Markup :: XML",
+                   "Topic :: Software Development :: Libraries :: Python Modules",
+                  ],
+      packages=['minixsv', 'genxmlif'],
       package_data={'minixsv': ['datatypes.xsd',
-                        'xml.xsd',
-                        'XMLSchema.xsd',
-                        'XMLSchema-instance.xsd',
-                        'XInclude.xsd',
-                        'README.txt',
-                        'minixsv'],
+                                'xml.xsd',
+                                'XMLSchema.xsd',
+                                'XMLSchema-instance.xsd',
+                                'XInclude.xsd',
+                                'README.txt',
+                                'minixsv'],
                      'genxmlif':['README.txt']},
      )
